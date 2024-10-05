459e9a1757dc99be3dc3c26de04cb6247dd79a17
meetvivek <kumarvivek.july28@gmail.com>
2024-10-06 00:42:04 +0530

    Create a model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from uuid import uuid4
+import uuid
+from statistics import mode
+import os
 
-# Create your models here.
+class Folder(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid4.uuid)
+    created_at = models.DateTimeField(auto_now=True)
+
+def get_upload_path(instance, filename):
+    return os.path.join(str(instance.folder.uid), filename)
+
+class Files(models.Model):
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    file = models.FileField(upload_to=get_upload_path)
+    created_at = models.DateTimeField(auto_now=True)
